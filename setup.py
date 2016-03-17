4bc58c16b71a042bb67acc497eeb133e337d8b10
Gérald Lelong <gerald.lelong@easymov.fr>
2016-03-17 14:27:22 +0100

    renamed to spekti

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
  # -*- coding: utf-8 -*-
 
 setup(
-    name = 'watcher',
+    name = 'spekti',
     version = '0.1.0',
     description = 'Execute command on file or folder change',
     author = 'Gérald Lelong',
     author_email = 'lelong.gerald@gmail.com',
     url = 'https://github.com/lelongg/watcher',
     scripts = [
-        'watcher',
+        'spekti',
     ],
     install_requires = [
         'watchdog',
